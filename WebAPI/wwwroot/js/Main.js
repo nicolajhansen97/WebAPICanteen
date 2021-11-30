22a9fbb1873ccd8192efdcd5d06739ddb88fac91
Khorp <rasm523c@easv365.dk>
2021-11-30 12:09:28 +0100

    Json desgin. in breakfast

--- WebAPI/wwwroot/js/Main.js
+++ WebAPI/wwwroot/js/Main.js
@@ -19,18 +19,45 @@ function idleLogout() {
 }
 
 
-function getTable(tableName) {
-   
-
-    var host = 'https://localhost:5001/api/'
+async function getTable(tableName) {
+   var host = 'https://localhost:5001/api/'
     host = host + tableName;
-    $.ajax({
+    const data =  await $.ajax({
         type: "GET",
         dataType: "json",
         url: host
     }).then(function (data) {
-        alert(JSON.stringify(data))
+        return data
     })
+
+    return data
 }
+
+async function test(tableName) {
+
+    const data = await getTable(tableName)
+
+    document.getElementById('ITEMS').innerHTML = `
+        <h1 class="B-Title">BreakFast Items</h1>
+        <p>${data.map(function (food) {
+            return `
+                <p>${food.fldItemname}</p>
+                <img src="${getImageUrl(food.fldImage)}" alt="FOOD PIC">
+                <p>${food.fldItemDescription}</p><br>
+                <p>${food.fldPrice} kr</p>
+                <button>add to cart</button>
+            `
+
+        }).join('')}</p>
+    `
+    console.log(data)
+}
+
+function getImageUrl(picName) {
+    //alert(picName)
+    return ""
+}
+
+
 
 
